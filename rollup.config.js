87d7ba06c04a4e762051d6ad6abde4dd1e7c8d66
Amphiluke <amphiluke@gmail.com>
2020-05-24 16:02:37 +0700

    Setup project base

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import vue from "rollup-plugin-vue";
+import {terser} from "rollup-plugin-terser";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/main.js",
+  output: {
+    file: "dist/main.js",
+    format: "esm"
+  },
+  plugins: [
+    vue({
+      template: {
+        isProduction: true,
+        compilerOptions: {
+          preserveWhitespace: false
+        }
+      }
+    }),
+    replace({
+      "process.env.NODE_ENV": "\"production\""
+    }),
+    resolve(),
+    terser()
+  ]
+};
---
+++ src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    hi
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="less">
+  @import (inline) "./node_modules/normalize.css/normalize.css";
+</style>
