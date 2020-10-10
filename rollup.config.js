b756b12d78be3e01541f37663bd425a293ca4704
Amphiluke <amphiluke@gmail.com>
2020-10-10 14:59:14 +0700

    Add build stage for service worker

--- rollup.config.js
+++ rollup.config.js
@@ -3,25 +3,45 @@ import {terser} from "rollup-plugin-terser";
 import replace from "@rollup/plugin-replace";
 import resolve from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "src/main.js",
-  output: {
-    file: "dist/main.js",
-    format: "esm"
-  },
-  plugins: [
-    vue({
-      template: {
-        isProduction: true,
-        compilerOptions: {
-          preserveWhitespace: false
+import {createRequire} from "module";
+let require = createRequire(import.meta.url);
+let pkg = require("./package.json");
+
+export default [
+  {
+    input: "src/main.js",
+    output: {
+      file: "dist/main.js",
+      format: "esm"
+    },
+    plugins: [
+      vue({
+        template: {
+          isProduction: true,
+          compilerOptions: {
+            preserveWhitespace: false
+          }
         }
-      }
-    }),
-    replace({
-      "process.env.NODE_ENV": `"${process.env.BUNDLE_TYPE || "production"}"`
-    }),
-    resolve(),
-    terser()
-  ]
-};
+      }),
+      replace({
+        "process.env.NODE_ENV": `"${process.env.BUNDLE_TYPE || "production"}"`
+      }),
+      resolve(),
+      terser()
+    ]
+  },
+  {
+    input: "sw.src.js",
+    output: {
+      file: "sw.js",
+      format: "esm"
+    },
+    plugins: [
+      replace({
+        PACKAGE_VERSION: pkg.version,
+        delimiters: ["{{", "}}"]
+      }),
+      terser()
+    ]
+  }
+];
