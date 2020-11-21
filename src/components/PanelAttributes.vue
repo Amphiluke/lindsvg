fa9b5c7f414b8b52259edd401a40f1a29c0fba11
Amphiluke <amphiluke@gmail.com>
2020-11-21 23:26:21 +0700

    Add attribute selector

--- src/components/PanelAttributes.vue
+++ src/components/PanelAttributes.vue
@@ -45,6 +45,28 @@
                 placeholder="Enter attribute name…"
               >
             </label>
+            <label class="attribute-list-ctrl">
+              <select
+                class="attribute-list"
+                title="Select attribute"
+                @change="newAttributeName = $event.target.value"
+              >
+                <optgroup
+                  v-for="(attributeList, group) in attributes"
+                  :key="group"
+                  :label="group"
+                >
+                  <option
+                    v-for="attribute in attributeList"
+                    :key="attribute"
+                    :value="attribute"
+                    :disabled="$store.state.attributes.hasOwnProperty(attribute)"
+                  >
+                    {{ attribute }}
+                  </option>
+                </optgroup>
+              </select>
+            </label>
             <button
               type="button"
               class="add-attribute-button icon-button icon-button-add"
@@ -89,7 +111,100 @@ export default {
 
   data: () => ({
     newAttributeName: "",
-    newAttributeValue: ""
+    newAttributeValue: "",
+    attributes: {
+      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Presentation
+      "Presentation Attributes": [
+        "alignment-baseline",
+        "baseline-shift",
+        "clip",
+        "clip-path",
+        "clip-rule",
+        "color",
+        "color-interpolation",
+        "color-interpolation-filters",
+        "color-profile",
+        "color-rendering",
+        "cursor",
+        "direction",
+        "display",
+        "dominant-baseline",
+        "enable-background",
+        "fill",
+        "fill-opacity",
+        "fill-rule",
+        "filter",
+        "flood-color",
+        "flood-opacity",
+        "font-family",
+        "font-size",
+        "font-size-adjust",
+        "font-stretch",
+        "font-style",
+        "font-variant",
+        "font-weight",
+        "glyph-orientation-horizontal",
+        "glyph-orientation-vertical",
+        "image-rendering",
+        "kerning",
+        "letter-spacing",
+        "lighting-color",
+        "marker-end",
+        "marker-mid",
+        "marker-start",
+        "mask",
+        "opacity",
+        "overflow",
+        "pathLength",
+        "pointer-events",
+        "shape-rendering",
+        "solid-color",
+        "solid-opacity",
+        "stop-color",
+        "stop-opacity",
+        "stroke",
+        "stroke-dasharray",
+        "stroke-dashoffset",
+        "stroke-linecap",
+        "stroke-linejoin",
+        "stroke-miterlimit",
+        "stroke-opacity",
+        "stroke-width",
+        "text-anchor",
+        "text-decoration",
+        "text-rendering",
+        "transform",
+        "unicode-bidi",
+        "vector-effect",
+        "visibility",
+        "word-spacing",
+        "writing-mode"
+      ],
+
+      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Core
+      "Core Attributes": [
+        "id",
+        "lang",
+        "tabindex",
+        "xml:base",
+        "xml:lang",
+        "xml:space"
+      ],
+
+      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Styling
+      "Styling Attributes": [
+        "class",
+        "style"
+      ],
+
+      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/Conditional_Processing
+      "Conditional Processing Attributes": [
+        "externalResourcesRequired",
+        "requiredExtensions",
+        "requiredFeatures",
+        "systemLanguage"
+      ]
+    }
   }),
 
   methods: {
@@ -167,6 +282,31 @@ export default {
     input[type="text"] {
       text-align: left;
     }
+  }
+  .attribute-list-ctrl {
+    display: flex;
+    flex-shrink: 0;
+    position: relative;
+    &::before {
+      background: url(images/icons.svg) -150px 0 no-repeat;
+      content: "";
+      height: 25px;
+      left: 50%;
+      opacity: 0.5;
+      position: absolute;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      width: 25px;
+    }
+    &:hover::before,
+    &:focus-within::before {
+      opacity: 1;
+    }
+  }
+  .attribute-list {
+    cursor: pointer;
+    opacity: 0.01;
+    width: 32px;
   }
   .add-attribute-button {
     flex-shrink: 0;
