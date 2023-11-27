ff9d522cece4813de1ac0c844f8434e4ce89d0c5
Amphiluke <amphiluke@gmail.com>
2023-11-27 15:24:17 +0700

    Add the deploy workflow

--- src/components/PanelAbout.vue
+++ src/components/PanelAbout.vue
@@ -17,7 +17,7 @@ import panelStyles from "../styles/panel.module.css";
         This web application is a simple SVG-based L-system plotter and manager designed to serve as a live demonstration for the <a href="https://github.com/Amphiluke/lindsvg" target="_blank" rel="noopener">lindsvg</a> module which is used by the app under the hood.
       </p>
       <p>
-        There are a few L-system collections built in the app for demonstration purposes. These L-systems were gathered from various sources including the web, books, and articles. I appreciate the authors of these L-systems (you may find their names <a href="https://github.com/Amphiluke/lindsvg/blob/gh-pages/src/stores/bank.js" target="_blank" rel="noopener">in the app sources</a>).
+        There are a few L-system collections built in the app for demonstration purposes. These L-systems were gathered from various sources including the web, books, and articles. I appreciate the authors of these L-systems (you may find their names <a href="https://github.com/Amphiluke/lindsvg/blob/pwa/src/stores/bank.mjs" target="_blank" rel="noopener">in the app sources</a>).
       </p>
       <h3>Links</h3>
       <ul>
@@ -28,7 +28,7 @@ import panelStyles from "../styles/panel.module.css";
           <a href="https://github.com/Amphiluke/lindsvg" target="_blank" rel="noopener">The lindsvg module sources</a>
         </li>
         <li>
-          <a href="https://github.com/Amphiluke/lindsvg/tree/gh-pages" target="_blank" rel="noopener">The app sources</a>
+          <a href="https://github.com/Amphiluke/lindsvg/tree/pwa" target="_blank" rel="noopener">The app sources</a>
         </li>
       </ul>
       <hr>
